03be02d86e0a13c1386886da179903bd98c7611f
Jason Vranek <jvranek@ucsc.edu>
2023-04-11 22:27:28 -0700

    Add integration tests to verify can sign AggregationSlot objects from eth2 specs

--- tests/signing_tests/mod.rs
+++ tests/signing_tests/mod.rs
@@ -3,7 +3,7 @@ pub mod block;
 pub mod block_v2;
 pub mod randao_reveal;
 pub mod aggregate_and_proof;
-// pub mod aggregation_slot;
+pub mod aggregation_slot;
 // pub mod deposit;
 // pub mod sync_committee_message;
 // pub mod sync_committee_selection_proof;
