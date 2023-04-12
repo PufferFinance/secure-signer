1dc206e11ad8be8d3434516833b7be49038d58de
Jason Vranek <jvranek@ucsc.edu>
2023-04-11 22:38:26 -0700

    Add integration tests to verify can sign SyncAggregatorSelectionData objects from eth2 specs

--- tests/signing_tests/mod.rs
+++ tests/signing_tests/mod.rs
@@ -6,6 +6,6 @@ pub mod aggregate_and_proof;
 pub mod aggregation_slot;
 pub mod deposit;
 pub mod sync_committee_message;
-// pub mod sync_committee_selection_proof;
+pub mod sync_committee_selection_proof;
 // pub mod contribution_and_proof;
 // pub mod validator_registration;
