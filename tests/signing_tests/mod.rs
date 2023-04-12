8bb11641bc0108c46fde7576d95793ff9da8aec7
Jason Vranek <jvranek@ucsc.edu>
2023-04-11 22:30:42 -0700

    Add integration tests to verify can sign DepositMessage objects from eth2 specs

--- tests/signing_tests/mod.rs
+++ tests/signing_tests/mod.rs
@@ -4,7 +4,7 @@ pub mod block_v2;
 pub mod randao_reveal;
 pub mod aggregate_and_proof;
 pub mod aggregation_slot;
-// pub mod deposit;
+pub mod deposit;
 // pub mod sync_committee_message;
 // pub mod sync_committee_selection_proof;
 // pub mod contribution_and_proof;
