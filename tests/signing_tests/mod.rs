ab88a0751e07270a23d0165039873013d15f3b80
Jason Vranek <jvranek@ucsc.edu>
2023-04-11 22:25:38 -0700

    Add integration tests to verify can sign AggregateAndProof objects from eth2 specs

--- tests/signing_tests/mod.rs
+++ tests/signing_tests/mod.rs
@@ -2,7 +2,7 @@ pub mod attestation;
 pub mod block;
 pub mod block_v2;
 pub mod randao_reveal;
-// pub mod aggregate_and_proof;
+pub mod aggregate_and_proof;
 // pub mod aggregation_slot;
 // pub mod deposit;
 // pub mod sync_committee_message;
