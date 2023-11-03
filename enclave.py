3d22e9f0dcaf29bf0e9bc40e1f8aa10e929bad67
Jason <jvranek@ucsc.edu>
2023-11-03 04:51:42 +0000

    Update unit test command

--- enclave.py
+++ enclave.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import os
-import sys
 import subprocess
 import argparse
+import json
 
 OPENSSL_DIR = "/usr/local/occlum/x86_64-linux-musl/"
 
@@ -31,7 +31,7 @@ def measure(enclave_path):
 def compile_rust(binary_name, build_flags):
     if using_enclave:
         subprocess.run(["./build_epid_ra.sh"])
-    os.environ["OPENSSL_DIR"] = OPENSSL_DIR
+        os.environ["OPENSSL_DIR"] = OPENSSL_DIR
     cmd = [cargo_bin, "build", "--bin", binary_name]
     cmd.extend(build_flags)
     subprocess.run(cmd)
@@ -43,7 +43,7 @@ def new_instance(enclave_name, enclave_path):
     os.chdir(enclave_path)
     subprocess.run(["occlum", "init", enclave_name])
     subprocess.run(["copy_bom", "-f", f"../conf/{build_type}-rust-config.yaml", "--root", "image", "--include-dir", "/opt/occlum/etc/template"])
-    subprocess.run(["cp", f"../conf/{ra_network_config_name}", "./image/etc/"])
+    subprocess.run(["cp", f"../conf/ra_config.json", "./image/etc/"])
     subprocess.run(["cp", "/etc/resolv.conf", "./image/etc"])
     subprocess.run(["cp", "/etc/hosts", "./image/etc"])
     new_json = subprocess.check_output(["jq", '.resource_limits.user_space_size = "1024MB" | .resource_limits.kernel_space_heap_size="512MB" | .process.default_heap_size = "512MB" | .resource_limits.max_num_of_threads = 32 | .env.default = ["OCCLUM=yes", "RUST_LOG=info"] | .metadata.debuggable = false', "Occlum.json"]).decode("utf-8")
@@ -59,30 +59,42 @@ def run_enclave(binary_name, enclave_path, port, fork_version):
         subprocess.run([cargo_bin, "run", "--bin", binary_name, str(port), fork_version])
 
 def unit_tests():
-    os.environ["OPENSSL_DIR"] = OPENSSL_DIR
-    subprocess.run([cargo_bin, "test", "-F", "sgx", "--", "--test-threads", "1"])
+    if using_enclave:
+        os.environ["OPENSSL_DIR"] = OPENSSL_DIR
+    os.environ["SECURE_SIGNER_PORT"] = str(port)
+    subprocess.run([cargo_bin, "test", "--", "--test-threads", "1"])
+
+def read_fork_version(path):
+    try:
+        with open(path, 'r') as f:
+            c = json.load(f) 
+            return c['fork_info']['fork']['current_version']
+    except:
+        exit(f"Error loading config file: '{path}'")
 
 def process_args():
     parser = argparse.ArgumentParser(description="Build and containerize enclave.")
     parser.add_argument("-p", "--port", type=int, default=9001, help="Enclave port (default: 9001)")
-    parser.add_argument("-e", "--enclave", action="store_true", help="Build using Occlum")
+    parser.add_argument("-e", "--enclave", action="store_true", help="Build using occlum-cargo. If unset, builds and runs without SGX.")
     parser.add_argument("-s", "--secure-signer", action="store_true", help="Use Secure-Signer enclave")
     parser.add_argument("-g", "--guardian", action="store_true", help="Use Guardian enclave")
     parser.add_argument("-v", "--validator", action="store_true", help="Use Validator enclave")
     parser.add_argument("-b", "--build", action="store_true", help="Build from cached dependencies")
     parser.add_argument("-x", "--run", action="store_true", help="Run service on specified port or default")
     parser.add_argument("-t", "--unit-tests", action="store_true", help="Run unit tests")
-    parser.add_argument("-n", "--network", choices=["ephemery", "holesky", "mainnet"], default="ephemery", help="Network type (default: ephemery)")
-    parser.add_argument("-f", "--fork-version", default="0x00001020", help="Beacon chain fork version")
-    parser.add_argument("-m", "--measure", action="store_true", help="Measure")
+    parser.add_argument("-c", "--config", default="./conf/ephemery_network_config.json", help="Network config JSON")
+    parser.add_argument("-m", "--measure", action="store_true", help="Print the specified enclave's measurements")
     parser.add_argument("-o", "--output-package", action="store_true", help="Package the Occlum image for Dockerization")
     args = parser.parse_args()
 
     # global vars
-    global port, build_type, cargo_bin, using_enclave, build_flags, ra_network_config_name, package_enclave, fork_version
+    global port, build_type, cargo_bin, using_enclave, build_flags, network_config, package_enclave
     port = args.port
-    ra_network_config_name = f"{args.network}_config.json"
+    network_config = args.config
     package_enclave = args.output_package
+
+    # Read fork version from network config
+    fork_version = read_fork_version(network_config)
 
     # Build type
     using_enclave = args.enclave
@@ -128,16 +140,16 @@ def process_args():
     # Run the enclave
     if args.run:
         if build_type == "secure-signer":
-            run_enclave("secure-signer", f"{script_dir}/Secure-Signer", port, args.fork_version)
+            run_enclave("secure-signer", f"{script_dir}/Secure-Signer", port, fork_version)
         elif build_type == "guardian":
-            run_enclave("guardian", f"{script_dir}/Guardian", port, args.fork_version)
+            run_enclave("guardian", f"{script_dir}/Guardian", port, fork_version)
         elif build_type == "validator":
-            run_enclave("validator", f"{script_dir}/Validator", port, args.fork_version)
+            run_enclave("validator", f"{script_dir}/Validator", port, fork_version)
         else:
             exit("Error: No valid run type specified. Use -v, -g, or -s first.")
     
-    # if args.unit_tests:
-    #     unit_tests()
+    if args.unit_tests:
+        unit_tests()
     
 if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.abspath(__file__))
